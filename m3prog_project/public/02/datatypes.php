85a7df0a01c2116b422fc5e8261de73db3c5b514
Nino <35476@ma-web.nl>
2024-03-08 09:29:08 +0100

    Les2 01

---
+++ m3prog_project/public/02/datatypes.php
@@ -0,0 +1,15 @@
+<?php
+
+$huisnummer = 307;
+$dlPassed = true;
+$priceLiter = 0.378;
+$hoursPerDay = 24;
+
+
+echo $huisnummer;
+echo "<br>";
+echo $dlPassed;
+echo "<br>";
+echo $priceLiter;
+echo "<br>";
+echo $hoursPerDay;
