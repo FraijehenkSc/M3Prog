058c72b8f11af6bd8939f7f01ab326e6abdd4d71
Nino <35476@ma-web.nl>
2024-03-08 09:40:13 +0100

    Les2 03

---
+++ m3prog_project/public/02/variabelen.php
@@ -0,0 +1,13 @@
+<?php
+
+$auto2 = "ja";
+$_naam = "ja";
+//$auto's = "ja";
+//$auto/onderdelen = "ja";
+//$huis&tuin = "ja";
+$naw = "ja";
+//$22 = "ja";
+$$auto = "ja";
+$kleurVanAuto = "ja";
+//$software-dev = "ja";
+
